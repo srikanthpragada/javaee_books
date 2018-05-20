51a392e7f35bc2d7626dcdb8e5f263787d93b2ed
Srikanth Pragada <srikanthpragada@gmail.com>
2018-05-20 20:09:03 +0530

    20-MAY-2018
    Use this script to create required database objects in Oracle Database

--- WebContent/WEB-INF/database.sql
+++ WebContent/WEB-INF/database.sql
@@ -9,10 +9,10 @@ create table books
 create sequence book_id_seq start with 1 nocache;
 
 insert into books values(book_id_seq.nextval, 'Java Complete Reference',
-  'Herbert Schild', 750);
+  'Herbert Schildt', 750);
   
-insert into books values(book_id_seq.nextval, 'C Programming',
-  'Ritchie',350);
+insert into books values(book_id_seq.nextval, 'The C Programming Language',
+  'Dennis Ritchie',350);
   
 insert into books values(book_id_seq.nextval, 'Oracle Database SQL',
   'Jason Price',850);    
